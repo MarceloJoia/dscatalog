1a04959426c34adcc15907e1d7885aaf62804cdd
MarceloJoia <sitejoia@hotmail.com>
2021-11-13 16:22:50 -0300

    DTO

--- backend/src/main/java/br/com/joiamarketing/dscatalog/resources/CategoryRecource.java
+++ backend/src/main/java/br/com/joiamarketing/dscatalog/resources/CategoryRecource.java
@@ -8,22 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.joiamarketing.dscatalog.entities.Category;
+import br.com.joiamarketing.dscatalog.dto.CategoryDTO;
 import br.com.joiamarketing.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
-public class Resource {
+public class CategoryRecource {
 	
 	@Autowired
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findall() {
-		List<Category> list = service.findAll();
-		//List<Category> list = new ArrayList<>();
-		//list.add(new Category(1L, "Book"));
-		//list.add(new Category(2L, "Electonics"));
+	public ResponseEntity<List<CategoryDTO>> findall() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
