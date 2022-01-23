2d1026cc7c15e4953bd60f2944e6172c08b2a99d
MarceloJoia <sitejoia@hotmail.com>
2022-01-23 18:16:33 -0300

    Custom Constraintvalidator e-mail

---
+++ backend/src/main/java/br/com/joiamarketing/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,45 @@
+package br.com.joiamarketing.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.joiamarketing.dscatalog.dto.UserInsertDTO;
+import br.com.joiamarketing.dscatalog.entities.User;
+import br.com.joiamarketing.dscatalog.repositories.UserRepository;
+import br.com.joiamarketing.dscatalog.resources.exceptions.FieldMessage;
+
+@UserInsertValid
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+	
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = repository.findByEmail(dto.getEmail());
+		if(user != null) {
+			list.add(new FieldMessage("email", "Este email já está cadastrado."));
+		}
+		
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage())
+			.addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
